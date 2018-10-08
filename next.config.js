f3791704f2cc124344b41f7e2c7068dcc0cca049
Tran Quoc Khanh <khanhtran@QKT-PC.net.fpt>
2018-10-08 12:37:02 +0700

    Add build folder

---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    // some configuration
+    assetPrefix: process.env.NODE_ENV === 'production' ? 'https://khanhtrancse.github.io/my-blog' : '',
+    // another configuration
+  }
