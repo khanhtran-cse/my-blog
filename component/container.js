e2be40e7ad68b00bc6eb78fd9df421b7152d2590
Tran Quoc Khanh <khanhtran@QKT-PC.net.fpt>
2018-10-09 02:01:57 +0700

    Add deploy command

--- component/container.js
+++ component/container.js
@@ -26,7 +26,7 @@ export default class extends React.Component {
                 <Head>
                     <title>{this.props.title} </title>
 
-                    <meta charset="utf-8" />
+                    <meta charSet="utf-8" />
                     <meta name="viewport" content="initial-scale=1.0, width=device-width" />
                     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" />
                     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
