24cb7d84ee6b569653f455726a0e5341a0930561
Tran Quoc Khanh <khanhtran@QKT-PC.net.fpt>
2018-10-10 00:34:16 +0700

    Update deploy version

--- pages/post.js
+++ pages/post.js
@@ -19,33 +19,33 @@ export default class extends React.Component {
     componentDidMount() {
         const myUrl = url.parse(window.location.href);
 
-        if (myUrl.pathname == config.PATH.POST) {
+        console.log(myUrl.pathname);
+        console.log('local path', config.PATH.POST);
+        
+        //Get id of target post
+        const query = myUrl.search;
+        const regex = /id=[0-9]*/g
+        const idString = query.match(regex)[0];
+        const id = idString.slice(3, idString.length);
+        console.log(query);
+        console.log('Get information of post has id', id);
 
-            //Get id of target post
-            const query = myUrl.search;
-            const regex = /id=[0-9]*/g
-            const idString = query.match(regex)[0];
-            const id = idString.slice(3, idString.length);
-            console.log(query);
-            console.log('Get information of post has id',id);
+        //Get data of target post
+        const content = Detail.getDetail(id);
+        const meta = Data.getPost(id);
 
-            //Get data of target post
-            const content = Detail.getDetail(id);
-            const meta = Data.getPost(id);
-
-            console.log('data',content,meta);
-            if (content && meta) {
-                this.setState({
-                    meta,
-                    content
-                })
-            } else {
-                console.log('error',content,meta);
-                this.setState({
-                    meta: {name: Lan.t('title_error')},
-                    content: null
-                })
-            }
+        console.log('data', content, meta);
+        if (content && meta) {
+            this.setState({
+                meta,
+                content
+            })
+        } else {
+            console.log('error', content, meta);
+            this.setState({
+                meta: { name: Lan.t('title_error') },
+                content: null
+            })
         }
     }
 
